8c9f0ab0e71c6f55e6866bbc916e7bd0c0863f42
Merdan <wisew.tube@gmail.com>
2026-02-13 00:44:06 +0500

    init commit

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,14 @@
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
+const { withNativeWind } = require("nativewind/metro");
 
 /**
  * Metro configuration
  * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+const defaultConfig = getDefaultConfig(__dirname);
+
+const config = mergeConfig(defaultConfig, {
+});
+
+module.exports = withNativeWind(config, { input: "./global.css" });
