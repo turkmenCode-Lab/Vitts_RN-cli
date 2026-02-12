d9cf98ef9e35a5e9ee5e5fc2ffdae21c8b94c87a
Merdan <wisew.tube@gmail.com>
2026-02-13 01:40:51 +0500

    init commit

--- android/app/src/main/java/com/vitts/MainApplication.kt
+++ android/app/src/main/java/com/vitts/MainApplication.kt
@@ -1,27 +1,40 @@
 package com.vitts
 
 import android.app.Application
+import android.content.res.Configuration
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
-import com.facebook.react.ReactHost
-import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
-import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
+import com.facebook.react.ReactNativeHost
+import com.facebook.react.defaults.DefaultReactNativeHost
+import expo.modules.ApplicationLifecycleDispatcher
+import expo.modules.ReactNativeHostWrapper
 
 class MainApplication : Application(), ReactApplication {
 
-  override val reactHost: ReactHost by lazy {
-    getDefaultReactHost(
-      context = applicationContext,
-      packageList =
-        PackageList(this).packages.apply {
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // add(MyReactNativePackage())
-        },
-    )
-  }
+    // Wrap the default ReactNativeHost with Expo's wrapper
+    override val reactNativeHost: ReactNativeHost by lazy {
+        ReactNativeHostWrapper(
+            this,
+            object : DefaultReactNativeHost(this) {
+                override fun getPackages() = PackageList(this).packages.apply {
+                    // Manually add packages that cannot be autolinked here
+                    // add(MyCustomPackage())
+                }
 
-  override fun onCreate() {
-    super.onCreate()
-    loadReactNative(this)
-  }
+                override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+                override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+            }
+        )
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        loadReactNative(this)
+        ApplicationLifecycleDispatcher.onApplicationCreate(this)
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        ApplicationLifecycleDispatcher.onConfigurationChanged(this, newConfig)
+    }
 }
